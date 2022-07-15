8e33403871c96838d8be8ed39e8bbcb9541f6037
“emilykiss” <emilylanekiss@gmail.com>
2022-07-15 15:59:47 -0700

    merge conflict

--- controllers/tags.js
+++ controllers/tags.js
@@ -4,32 +4,31 @@ const db = require("../models")
 
 // GET ALL the tags
 router.get("/", async (req, res) => {
-	try {
-		// figure out how to get current user from front end to save relationship
-		const allTags = await db.Tag.find({})
-		res.json(allTags)
-	} catch (error) {
-		if (error.name === "ValidationError") {
-			res.status(400).json({ msg: error.message })
-		} else {
-			res.status(500).json({ msg: "server error" })
-		}
-	}
+  try {
+    const allTags = await db.Tag.find({})
+    res.send(allTags)
+  } catch (error) {
+    if (error.name === "ValidationError") {
+      res.status(400).json({ msg: error.message })
+    } else {
+      res.status(500).json({ msg: "server error" })
+    }
+  }
 })
 
 // POST a tags
 router.post("/", async (req, res) => {
-	try {
-		// Add a tag to the DB
-		const newTag = await db.Tag.create(req.body) 
-		res.status(201).json(newTag)
-	} catch (error) {
-		if (error.name === "ValidationError") {
-			res.status(400).json({ msg: error.message })
-		} else {
-			res.status(500).json({ msg: "server error" })
-		}
-	}
+  try {
+    // Add a tag to the DB
+    const newTag = await db.Tag.create(req.body) //maybe move to profile?
+    res.status(201).json(newTag)
+  } catch (error) {
+    if (error.name === "ValidationError") {
+      res.status(400).json({ msg: error.message })
+    } else {
+      res.status(500).json({ msg: "server error" })
+    }
+  }
 })
 
 // DELETE a tag
