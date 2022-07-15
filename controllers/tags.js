448175f60a602d898556a53116ac0c08bdd6059b
“emilykiss” <emilylanekiss@gmail.com>
2022-07-15 15:51:28 -0700

    merge conflict

--- controllers/tags.js
+++ controllers/tags.js
@@ -35,17 +35,17 @@ router.post("/", async (req, res) => {
 
 // DELETE a tag
 router.delete("/:id", async (req, res) => {
-	try {
-		const id = req.params.id
-		await db.Tag.findByIdAndDelete(id)
-		res.sendStatus(204)
-	} catch (error) {
-		if (error.name === "ValidationError") {
-			res.status(400).json({ msg: error.message })
-		} else {
-			res.status(500).json({ msg: "server error" })
-		}
-	}
+  try {
+    const id = req.params.id
+    await db.Tag.findByIdAndDelete(id)
+    res.sendStatus(204)
+  } catch (error) {
+    if (error.name === "ValidationError") {
+      res.status(400).json({ msg: error.message })
+    } else {
+      res.status(500).json({ msg: "server error" })
+    }
+  }
 })
 
 // GET /tags/:id specific tag
